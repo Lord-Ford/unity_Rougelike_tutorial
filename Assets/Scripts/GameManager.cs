b69c3e62203b292800ce02c371c1504f1c2e35b6
Lord-Ford <isiah.f96@gmail.com>
2015-08-20 02:53:06 -0400

    firs commit, 3rd try

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public BordManager bordScript;
+
+	private int level = 3;
+	
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		bordScript = GetComponent<BordManager> ();
+		InitGame ();
+	}
+
+	void InitGame ()
+	{
+		bordScript.SetupScene (level);
+	}
+}
